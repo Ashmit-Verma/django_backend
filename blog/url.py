b8fc2cc39236989ddab975ebd756cf950a8c200e
Ashmit Verma <a1verma8105@gmail.com>
2023-08-12 18:44:27 +0530

    commit all

---
+++ blog/url.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('page1/', views.page1, name="page-1"),
+    path('page2/', views.page2, name="page-2")
+]
---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+def page1(request):
+    return render(request, 'blog/page1.html')
+
+
+def page2(request):
+    return render(request, 'blog/page2.html')
